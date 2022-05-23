3904dbc3763ff2bacd1d3ce69436903651d1f0de
HeathersCodes <105893866+HeathersCodes@users.noreply.github.com>
2022-05-23 00:37:15 -0400

    Create practice.sql

---
+++ Week 13: BNF and SQL/practice.sql
@@ -0,0 +1,4 @@
+create table cities as
+  select 38 as latitude, 122 as longitude, "Berkeley" as name union
+  select 42,             71,               "Cambridge"        union
+  select 45,             93,               "Minneapolis";
