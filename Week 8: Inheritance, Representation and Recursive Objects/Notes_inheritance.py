bde3d970b8ed1901d3a264a15b675503088e1bbe
HeathersCodes <105893866+HeathersCodes@users.noreply.github.com>
2022-05-26 22:47:47 -0400

    Create Notes_inheritance.py

---
+++ Week 8: Inheritance, Representation and Recursive Objects/Notes_inheritance.py
@@ -0,0 +1,55 @@
+class Account:
+    """An account has a balance and a holder.
+
+    >>> a = Account('John')
+    >>> a.holder
+    'John'
+    >>> a.deposit(100)
+    100
+    >>> a.withdraw(90)
+    10
+    >>> a.withdraw(90)
+    'Insufficient funds'
+    >>> a.balance
+    10
+    >>> a.interest
+    0.02
+    """
+
+    interest = 0.02  # A class attribute
+
+    def __init__(self, account_holder):
+        self.holder = account_holder
+        self.balance = 0
+
+    def deposit(self, amount):
+        """Add amount to balance."""
+        self.balance = self.balance + amount
+        return self.balance
+
+    def withdraw(self, amount):
+        """Subtract amount from balance if funds are available."""
+        if amount > self.balance:
+            return 'Insufficient funds'
+        self.balance = self.balance - amount
+        return self.balance
+
+
+class CheckingAccount(Account):
+    """A bank account that charges for withdrawals.
+
+    >>> ch = CheckingAccount('Jack')
+    >>> ch.balance = 20
+    >>> ch.withdraw(5)
+    14
+    >>> ch.interest
+    0.01
+    """
+
+    withdraw_fee = 1
+    interest = 0.01
+
+    def withdraw(self, amount):
+        return Account.withdraw(self, amount + self.withdraw_fee)
+        # Alternatively:
+        return super().withdraw(amount + self.withdraw_fee)
